e8506c49597d502ffdb5a90602974fa53f11d59b
BSJJC <805086021@qq.com>
2023-07-27 23:51:48 +0800

    feat: Component Top done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="body">
     <Top></Top>
   </div>
 </template>
@@ -9,9 +9,7 @@ import Top from "./components/Top/index.vue"
 </script>
   
 <style>
-.dsadsadsa {
-  width: 100vw;
-  height: 100vh;
-  background-image: linear-gradient(135deg, #bdffe9 25%, #f1ffe8 25%, #f1ffe8 50%, #bdffe9 50%, #bdffe9 75%, #f1ffe8 75%, #f1ffe8 100%);
+#body {
+  background-image: url(./assets/imgs/body-bg.png);
 }
 </style>
--- src/components/Top/index.vue
+++ src/components/Top/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="w-[300px] h-[300px] bg-red-300">123
+  <div id="container" class="flex justify-center items-center relative">
+    <div class="w-[95%] my-3 relative">
+      <!-- logo -->
+      <img src="../../assets/imgs/logo.png" alt="pmarusama logo" class="absolute top-5 left-7 w-[48%]">
+
+      <!-- index visual -->
+      <img src="../../assets/imgs/index-visual.png" alt="index visual image" class="">
+    </div>
   </div>
 </template>
   
@@ -7,4 +14,10 @@
 
 </script>
   
-<style></style>
+<style>
+#container {
+  width: 100vw;
+  background-size: 50px 50px;
+  background-image: linear-gradient(135deg, #bdffe9 25%, #f1ffe8 25%, #f1ffe8 50%, #bdffe9 50%, #bdffe9 75%, #f1ffe8 75%, #f1ffe8 100%);
+}
+</style>
