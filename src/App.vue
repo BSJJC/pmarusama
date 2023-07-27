5d1cf497315200f2332bfbdadfbf6c156531b655
BSJJC <805086021@qq.com>
2023-07-27 23:26:35 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Top></Top>
+  </div>
+</template>
+  
+<script setup lang='ts'>
+import Top from "./components/Top/index.vue"
+</script>
+  
+<style>
+.dsadsadsa {
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(135deg, #bdffe9 25%, #f1ffe8 25%, #f1ffe8 50%, #bdffe9 50%, #bdffe9 75%, #f1ffe8 75%, #f1ffe8 100%);
+}
+</style>
---
+++ src/components/Top/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-[300px] h-[300px] bg-red-300">123
+  </div>
+</template>
+  
+<script setup lang='ts'>
+
+</script>
+  
+<style></style>
