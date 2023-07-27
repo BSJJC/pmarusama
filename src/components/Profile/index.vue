603f261229356f7d979605011278f078155f099b
BSJJC <805086021@qq.com>
2023-07-28 05:58:55 +0800

    feat: profile almost done

--- src/components/Profile/index.vue
+++ src/components/Profile/index.vue
@@ -1,31 +1,26 @@
 <template>
-  <div id="p-profile-container" class="relative my-10 bg-white">
+  <div id="p-profile-container" class="relative my-14">
 
-    <!-- profile title -->
-    <!-- <h2 class="w-[75vw] mb-10 flex justify-end items-center absolute -top-[5%]">
+    <!--profile title -->
+    <h2 class="w-[75vw] mb-10 flex justify-end items-center absolute -top-[5%]">
       <img src="../../assets/imgs/title-profile.png" alt="profile title" class="w-[250px] px-4 py-3">
     </h2>
 
-    <div id="profile-main" class="w-[95vw] ml-[5%] mt-[15%] pl-[10%] pt-[10%] pb-[15%]">
+    <div id="profile-main" class="w-[95%] h-full ml-[5%] p-14 pr-6 pt-12">
+      <div class="bg-[#ffda55]">
 
-      <div class="bg-[#ffda55] mt-[3%] mr-[5%] ml-[1%] p-4">
-
-        <div class="text-[#787878] leading-none font-bold mb-2">
-          <h3 class="text-[1.5rem] mb-2">P丸様。</h3>
+        <!-- profile intro -->
+        <div class="mb-3 font-bold text-[#787878] p-4">
+          <h3>P丸様。</h3>
           <span class="text-sm">YouTubeやTikTok等の動画投稿サイトで活動中のマルチエンターテイナー！</span>
           <br />
           <span class="text-sm">癖になる短編アニメ動画が大人気で、現在YouTubeチャンネルの登録者数は255万人、動画総再生数は20億回を突破。</span>
         </div>
 
+        <!-- profile image -->
         <div class="flex justify-end items-center">
           <img src="../../assets/imgs/profile-img.png" alt="profile image" class="w-[80%]">
         </div>
-      </div>
-    </div> -->
-
-    <div class="w-[95%] h-full ml-[5%]">
-      <div class="pt-[10%]">
-        <img src="../../assets/imgs/profile-bg-sp.png" alt="" class="h-full w-full">
       </div>
     </div>
 
@@ -42,7 +37,7 @@
   background-size: 150px;
 }
 
-/* h2 {
+h2 {
   background-image: url(../../assets/imgs/profile-bg2.png);
   background-size: 100% 100%;
 }
@@ -55,5 +50,5 @@ h3 {
 #profile-main {
   background-image: url(../../assets/imgs/profile-bg-sp.png);
   background-size: cover;
-} */
+}
 </style>
