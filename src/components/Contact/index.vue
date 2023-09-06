30facdf27377ab75aca54e64dc2914737b83eb85
BSJJC <805086021@qq.com>
2023-09-06 22:02:00 +0800

    feat: form data check

--- src/components/Contact/index.vue
+++ src/components/Contact/index.vue
@@ -17,15 +17,24 @@
             <div v-for="(i, index) in formData" :key="index" class="w-full flex items-center flex-col gap-2">
                 <div class=" relative w-full flex items-center gap-4 font-bold" style="font-family: 'M PLUS Rounded 1c';">
                     <div class="text-[#787878]">{{ i.title }}</div>
+
                     <div v-if="i.required" class="text-white bg-[#ff5889] px-2 rounded-full">必须</div>
+
+                    <Transition name="fade-up">
+                        <div v-show="i.verificationPassed === false"
+                            class="absolute right-[5px] text-white bg-[#ff5889] px-2 rounded-full">ダメです！</div>
+                    </Transition>
                 </div>
 
                 <input v-if="i.inputType === 'input'" type="text"
                     class="w-full border-[1.5px] border-[#00d991] rounded-lg p-2 text-sm focus:outline-none"
-                    v-model="i.text" @change="i.checkFunction">
+                    :style="{ borderColor: i.verificationPassed || i.verificationPassed === undefined ? '' : '#ff5889' }"
+                    v-model="i.text" @input="i.checkFunction" @blur="i.checkFunction">
 
-                <textarea v-else-if="i.inputType === 'textarea'" rows="10"
-                    class="w-full border-[1.5px] border-[#00d991] rounded-lg p-2 text-sm focus:outline-none"></textarea>
+                <textarea v-else-if="i.inputType === 'textarea'" v-model="i.text" rows="10"
+                    class="w-full border-[1.5px] border-[#00d991] rounded-lg p-2 text-sm focus:outline-none"
+                    :style="{ borderColor: i.verificationPassed || i.verificationPassed === undefined ? '' : '#ff5889' }"
+                    @input="i.checkFunction" @blur="i.checkFunction"></textarea>
             </div>
 
         </div>
@@ -34,6 +43,7 @@
   
 <script setup lang='ts'>
 import { ref, Ref } from "vue"
+import validateEmail from "../../utils/validateEmail"
 
 interface IFormData {
     title: string,
@@ -79,16 +89,38 @@ const formData: Ref<IFormData[]> = ref([
     }
 ])
 
+/**
+ * name check
+ */
 function nameCheck(): void {
+    if (formData.value[0].text.trim().length === 0) formData.value[0].verificationPassed = false
+    else formData.value[0].verificationPassed = true
 }
 
+/**
+ * subject check
+ */
 function subjectCheck(): void {
+    if (formData.value[1].text.trim().length === 0) formData.value[1].verificationPassed = false
+    else formData.value[1].verificationPassed = true
 }
 
+/**
+ * email adderess check
+ */
 function emailCheck(): void {
+    const email = formData.value[2].text.trim();
+
+    if (validateEmail(email)) formData.value[2].verificationPassed = true
+    else formData.value[2].verificationPassed = false
 }
 
+/**
+ * inquiry check
+ */
 function inquiryCheck(): void {
+    if (formData.value[3].text.trim().length === 0) formData.value[3].verificationPassed = false
+    else formData.value[3].verificationPassed = true
 }
 </script>
   
@@ -119,5 +151,16 @@ function inquiryCheck(): void {
     width: 100%;
     height: 6px;
     background: url(../../assets/imgs/section-bg-bottom.png) center bottom/auto 100%;
+}
+
+.fade-up-enter-active,
+.fade-up-leave-active {
+    transition: all 0.3s ease-in-out;
+}
+
+.fade-up-enter-from,
+.fade-up-leave-to {
+    opacity: 0;
+    transform: translateY(10px);
 }
 </style> 
