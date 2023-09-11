c420ef236b158f8b94fa09e7fb27a3ad85b689cc
BSJJC <805086021@qq.com>
2023-09-12 02:34:30 +0800

    feat: info componennt md:

--- src/components/Infomation/components/news.vue
+++ src/components/Infomation/components/news.vue
@@ -1,10 +1,10 @@
 <template>
   <li v-for="(i, index) in data" :key="index"
-    class="flex justify-center items-center w-[97%] mx-3 mb-4 border-l-[1px] border-[#f60]">
+    class="flex justify-center items-center w-[97%] mx-3 mb-4 border-l-[1px] border-[#f60] md:w-[90%]">
 
     <!-- news publish date -->
-    <div class="fredoka-one flex justify-center items-center w-[30%] text-[#f60] font-bold">
-      <div class="flex justify-center items-center flex-col text-[0.5rem] pr-2">
+    <div class="fredoka-one flex justify-center items-center w-[30%] text-[#f60] font-bold md:w-[15%]">
+      <div class="flex justify-center items-center flex-col text-[0.5rem] pr-2 md:text-[0.8rem]">
         <span class="w-full text-right leading-3">
           <!-- year -->
           {{ i.data.split("/")[0] }}
@@ -23,7 +23,7 @@
     </div>
 
     <!-- news title -->
-    <div class="m-plus-rounded-1c w-[70%]  border-l-[1px] border-r-[1px] border-[#f60] px-3">
+    <div class="m-plus-rounded-1c w-[70%]  border-l-[1px] border-r-[1px] border-[#f60] px-3 md:w-[85%]">
       <a href="" class="text-[#3865e0] font-bold">{{ i.title }}123</a>
     </div>
   </li>
--- src/components/Infomation/components/title.vue
+++ src/components/Infomation/components/title.vue
@@ -1,5 +1,5 @@
 <template>
-  <img src="../../../assets/imgs/title-info.png" alt="title information" class="w-[95%] mb-4">
+  <img src="../../../assets/imgs/title-info.png" alt="title information" class="w-[95%] mb-4 max-w-[550px]">
 </template>
   
 <script setup lang='ts'>
