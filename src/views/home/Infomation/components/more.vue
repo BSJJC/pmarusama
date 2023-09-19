adb0d715bab13b805ea1fb68c064ed0e1ccc6d86
BSJJC <805086021@qq.com>
2023-09-20 01:46:29 +0800

    feat: information more button enter aniamtion

--- src/views/home/Infomation/components/more.vue
+++ src/views/home/Infomation/components/more.vue
@@ -1,11 +1,31 @@
 <template>
-  <a href="" class="bg-[#ff5872] rounded-full py-2 px-8">
+  <a ref="more" href="" class="bg-[#ff5872] rounded-full py-2 px-8">
     <button class="m-plus-rounded-1c font-bold text-white text-[1.3rem]">もっと見る</button>
   </a>
 </template>
   
 <script setup lang='ts'>
+import { ref, Ref, onMounted } from "vue"
+import { useElementVisibility, watchOnce } from "@vueuse/core"
+import anime from "animejs";
 
+const more: Ref<HTMLElement | undefined> = ref()
+
+onMounted(() => {
+  const moreVisible = useElementVisibility(more)
+
+  watchOnce(
+    () => moreVisible.value,
+    () => {
+      // more button
+      anime({
+        targets: more.value,
+        delay: 600,
+        opacity: [0, 1]
+      })
+    }
+  )
+})
 </script>
   
 <style></style>
