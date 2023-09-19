006e3b5677eaff158183e043cf09ac83f313850b
BSJJC <805086021@qq.com>
2023-09-19 20:30:40 +0800

    feat: top enter animation

--- src/views/home/Top/index.vue
+++ src/views/home/Top/index.vue
@@ -1,16 +1,39 @@
 <template>
   <div id="p-top-container" class="flex justify-center items-center relative bg-fixed py-4 px-2">
     <div class="my-3 relative">
-      <img src="@/assets/imgs/logo.png" alt="pmarusama logo"
-        class="absolute top-[10%] left-[7%] w-[48%] md:top-[30%] md:w-[35%]">
+      <img ref="logo" src="@/assets/imgs/logo.png" alt="pmarusama logo"
+        class=" absolute z-[+1] top-[10%] left-[7%] w-[48%] md:top-[30%] md:w-[35%]">
 
-      <img src="@/assets/imgs/index-visual.png" alt="index visual image" class="lg:w-[82vw]">
+      <img ref="banner" src="@/assets/imgs/index-visual.png" alt="index visual image" class="lg:w-[82vw]">
     </div>
   </div>
 </template>
   
 <script setup lang='ts'>
+import { ref, Ref, onMounted } from "vue"
+import anime from 'animejs/lib/anime.es.js';
 
+const logo: Ref<HTMLElement | undefined> = ref()
+const banner: Ref<HTMLElement | undefined> = ref()
+
+onMounted(() => {
+  // logo animation
+  anime({
+    targets: logo.value,
+    delay: 300,
+    translateY: [200, 0],
+    opacity: [0, 1],
+    scale: [0.7, 1]
+  })
+
+  // banner animation
+  anime({
+    targets: banner.value,
+    duration: 1000,
+    opacity: [0, 1],
+    translateY: [100, 0],
+  })
+})
 </script>
   
 <style>
