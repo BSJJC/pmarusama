ee5c79ee42047f4af7a5f0b249d7ec951aff27a1
BSJJC <805086021@qq.com>
2023-09-24 20:49:01 +0800

    feat: dicographt enter animation

---
+++ src/api/discography/index.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export type DiscographyType = {
+    releaseDate: string,
+    name: string,
+    coverObjectIDs: string[],
+    stprLink: string,
+    songs: {
+        name: string,
+        credits: string
+    }[]
+}
+
+/**
+ * get discography data, includes title and coverID
+ * @returns discographies data
+ */
+export async function getDiscograhyData(): Promise<{
+    title: string,
+    coverID: string
+}[]> {
+    const discographyData: {
+        title: string,
+        coverID: string
+    }[] = []
+
+    await axios.get
+        ("http://localhost:5000/api/discography/get")
+        .then(res => {
+            const data = res.data.discographies
+
+            data.forEach((item: DiscographyType, index: number) => {
+                discographyData[index] = (
+                    {
+                        title: item.name,
+                        coverID: item.coverObjectIDs[0]
+                    }
+                )
+            });
+        })
+
+    return discographyData
+}
--- src/views/home/Discography/components/discographies.vue
+++ src/views/home/Discography/components/discographies.vue
@@ -1,82 +1,66 @@
 <template>
-  <div class="grid grid-cols-2 w-full mt-4 md:grid-cols-3 lg:max-w-[1060px] lg:mx-auto">
-    <div v-for="(i, index) in discographyData"
-      :key="index"
-      class="w-full h-full p-2">
-      <a href="#"
-        class="m-plus-rounded-1c block text-center  text-[0.8rem] font-bold text-[#6e5be4] md:text-[1rem]">
+    <div ref="discography"
+        class="grid grid-cols-2 w-full mt-4 md:grid-cols-3 lg:max-w-[1060px] lg:mx-auto">
+        <div v-for="(i, index) in discographyData"
+            :key="index"
+            class="w-full h-full p-2">
+            <a href="#"
+                class="m-plus-rounded-1c block text-center  text-[0.8rem] font-bold text-[#6e5be4] md:text-[1rem]">
 
-        <div v-if="discographyCoverUrls.length === discographyData.length"
-          class="w-full overflow-hidden mb-2 rounded-lg">
-          <img :src="discographyCoverUrls[index]"
-            alt="pmarusama discography cover"
-            class="w-full transition-all duration-300 hover:scale-110">
+                <div class="w-full overflow-hidden mb-2 rounded-lg">
+                    <img :src="`http://localhost:5000/api/discographyCover/${discographyData[index].coverID}`"
+                        alt="pmarusama discography cover"
+                        class="w-full transition-all duration-300 hover:scale-110">
+                </div>
+
+                {{ i.title }}
+            </a>
         </div>
-
-        {{ i.title }}
-      </a>
     </div>
-  </div>
 </template>
   
 <script setup lang='ts'>
-import { ref, Ref, onBeforeMount } from "vue"
-import axios from "axios"
-import type { DiscographyType } from "../types/discographyTypes"
+import { ref, Ref, onMounted } from "vue"
+import { useElementVisibility, watchOnce } from "@vueuse/core"
+import anime from "animejs";
+import { getDiscograhyData } from "@/api/discography/index.ts"
 
-interface IDiscographyStruct {
-  title: string,
-  coverID: string
-}
+const discography: Ref<HTMLElement | undefined> = ref()
+const discographyVisible: Ref<boolean> = useElementVisibility(discography)
 
-const discographyData: Ref<IDiscographyStruct[]> = ref([])
-const discographyCoverUrls: Ref<string[]> = ref([])
+const discographyData: Ref<{
+    title: string,
+    coverID: string
+}[]> = ref([])
 
-/**
- * get discographies data
- * @param callback the function that fetches the cover of discography
- */
-async function fetchData(callback: Function): Promise<void> {
-  await axios.get
-    ("http://localhost:5000/api/discography/get")
-    .then(res => {
-      const data = res.data.discographies
+onMounted(async () => {
+    await getDiscograhyData()
+        .then(res => {
+            discographyData.value = res
+        })
 
-      data.forEach((item: DiscographyType, index: number) => {
-        discographyData.value[index] = (
-          {
-            title: item.name,
-            coverID: item.coverObjectIDs[0]
-          }
-        )
-      });
-    })
-    .then(() => {
-      callback()
-    })
-}
+    watchOnce(
+        () => discographyVisible.value,
+        () => {
+            for (let i = 0; i < discography.value!.childNodes.length; i++) {
+                const el = discography.value!.childNodes[i];
 
-/**
- * get discography cover url by the coverID
- */
-function fetchCover(): void {
-  discographyData.value.forEach(item => {
-    axios.get(
-      `http://localhost:5000/api/discographyCover/${item.coverID}`
+                if (el.nodeName === "DIV") {
+                    const elAnimationConfig: anime.AnimeParams = {
+                        delay: 300 + 100 * i,
+                        opacity: [0, 1],
+                        translateY: [50, 0]
+                    }
+
+                    anime({
+                        targets: el,
+                        ...elAnimationConfig
+                    })
+                }
+            }
+        }
     )
-      .then(res => {
-        discographyCoverUrls.value.push(`data:image/jpeg;base64,${res.data}`)
-      })
-  });
-}
-
-async function init(): Promise<void> {
-  fetchData(fetchCover)
-}
-
-onBeforeMount(() => {
-  init()
 })
 </script>
   
-<style></style>../types/discographyTypes
+<style></style>
