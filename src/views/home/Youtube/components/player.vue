5faaf096705ff27ea5740c4b82559953c0538a2e
BSJJC <805086021@qq.com>
2023-09-24 12:53:04 +0800

    feat: youtube enter animation

--- src/views/home/Youtube/components/player.vue
+++ src/views/home/Youtube/components/player.vue
@@ -1,7 +1,9 @@
 <template>
-    <div id="p-youtube__main"
+    <div ref="outterContainer"
+        id="p-youtube__main"
         class="flex justify-center items-center relative z-[+1] mt-8 w-[96vw] py-[12vw] pr-[13vw] pl-[4vw] md:pt-[16vw] md:pr-[14vw] md:pb-[18vw] md:pl-[4vw] md:mt-0 lg:w-[80vw] lg:mr-auto lg:pt-[10vw] lg:pr-[18vw] lg:pb-[10vw] lg:pl-0 lg:justify-end">
-        <div id="p-youtube__main-inner"
+        <div ref="innerContainer"
+            id="p-youtube__main-inner"
             class="relative w-full bg-[#c0ecee] rounded-lg lg:max-w-[640px]">
             <div class=" max-w-none pt-[56.25%]">
                 <YoutubePlayer url="3V9952osjnc"
@@ -13,7 +15,43 @@
 </template>
   
 <script setup lang='ts'>
+import { ref, Ref, onMounted } from "vue"
+import { useElementVisibility, watchOnce } from "@vueuse/core"
+import anime from "animejs";
 import YoutubePlayer from "@/components/Common/YoutubePlayer.vue"
+import enterAnimation from "@/utils/enterAnimation";
+
+const outterContainer: Ref<HTMLElement | undefined> = ref()
+const innerContainer: Ref<HTMLTimeElement | undefined> = ref()
+
+const innerContainerVisible = useElementVisibility(innerContainer)
+
+const outterContainerAnimationConfig: anime.AnimeParams = {
+    delay: 200,
+    duration: 1000,
+    translateX: [-500, 0],
+    opacity: [0, 1],
+    easing: 'easeOutExpo'
+}
+
+const innerContainerAnimationConfig: anime.AnimeParams = {
+    delay: 600,
+    marginTop: [100, 16],
+    opacity: [0, 1],
+    easing: 'easeOutExpo'
+}
+
+onMounted(() => {
+    enterAnimation(outterContainer.value!, outterContainerAnimationConfig)
+    enterAnimation(innerContainer.value!, innerContainerAnimationConfig)
+
+    watchOnce(
+        () => innerContainerVisible.value,
+        () => {
+            innerContainer.value!.classList.add("show-before")
+        }
+    )
+})
 </script>
   
 <style scoped>
@@ -27,16 +65,29 @@ import YoutubePlayer from "@/components/Common/YoutubePlayer.vue"
     }
 }
 
-#p-youtube__main-inner::before {
+@keyframes show-before {
+    0% {
+        opacity: 0;
+    }
+
+    100% {
+        opacity: 1;
+    }
+}
+
+.show-before::before {
     position: absolute;
+    animation: show-before .3s ease-in-out forwards;
+    animation-delay: 1.5s;
+    opacity: 0;
     content: "";
     top: 5px;
     left: 5px;
     width: 100%;
     height: 100%;
+    z-index: -1;
     background-image: linear-gradient(135deg, #a7ef91 25%, #fff 25%, #fff 50%, #a7ef91 50%, #a7ef91 75%, #fff 75%, #fff 100%);
     background-size: 24px 24px;
-    z-index: -1;
     border-radius: 8px;
 }
 </style>
--- src/views/home/Youtube/components/title.vue
+++ src/views/home/Youtube/components/title.vue
@@ -1,14 +1,39 @@
 <template>
-    <div id="p-youtube__title"
+    <h2 ref="h2"
+        id="p-youtube__title"
         class="absolute right-0 z-[2] w-[85vw] flex justify-start items-center py-[10px] pr-0 pl-[4vw] md:w-[45vw] lg:w-[35vw] lg:h-[8vw] lg:pl-[2vw] lg:translate-y-[8vw]">
-        <img src="@/assets/imgs/title-youtube.png"
+        <img ref="img"
+            src="@/assets/imgs/title-youtube.png"
             alt="title youtube"
             class="w-[80%] max-w-[280px] lg:w-[70%]">
-    </div>
+    </h2>
 </template>
   
 <script setup lang='ts'>
+import { ref, Ref, onMounted } from "vue"
+import anime from "animejs";
+import enterAnimation from "@/utils/enterAnimation"
 
+const h2: Ref<HTMLElement | undefined> = ref()
+const img: Ref<HTMLImageElement | undefined> = ref()
+
+const h2AnimationConfig: anime.AnimeParams = {
+    dealy: 200,
+    translateX: ["100%", 0],
+    duration: 1000,
+    easing: 'easeOutExpo'
+}
+
+const imgAnimationConfig: anime.AnimeParams = {
+    delay: 400,
+    opacity: [0, 1],
+    translateY: [20, 0]
+}
+
+onMounted(() => {
+    enterAnimation(h2.value!, h2AnimationConfig)
+    enterAnimation(img.value!, imgAnimationConfig)
+})
 </script>
   
 <style scoped>
