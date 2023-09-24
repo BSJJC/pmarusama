4fd5249b152787c5beab047b73ad14cde1317c35
BSJJC <805086021@qq.com>
2023-09-24 12:25:26 +0800

    feat: profile enter animation

--- src/views/home/Profile/components/profile.vue
+++ src/views/home/Profile/components/profile.vue
@@ -9,14 +9,14 @@
             <div ref="intro"
                 class="m-plus-rounded-1c pb-3 text-[#787878] pt-6 px-4 leading-3 md:w-1/2 md:px-10 lg:p-0">
                 <h3 class="text-3xl font-bold">P丸様。</h3>
-                <span class=" text-sm font-bold md:text-lg">YouTubeやTikTok等の動画投稿サイトで活動中のマルチエンターテイナー！</span>
-                <br />
+                <span class="block text-sm font-bold md:text-lg">YouTubeやTikTok等の動画投稿サイトで活動中のマルチエンターテイナー！</span>
                 <span
-                    class=" text-sm font-bold md:text-lg">癖になる短編アニメ動画が大人気で、現在YouTubeチャンネルの登録者数は255万人、動画総再生数は20億回を突破。</span>
+                    class="block text-sm font-bold md:text-lg">癖になる短編アニメ動画が大人気で、現在YouTubeチャンネルの登録者数は255万人、動画総再生数は20億回を突破。</span>
             </div>
 
             <!-- profile image -->
-            <div class="flex justify-end items-center md:w-1/2">
+            <div ref="img"
+                class="flex justify-end items-center md:w-1/2">
                 <img src="@/assets/imgs/profile-img.png"
                     alt="profile image"
                     class="w-[60%] md:w-full">
@@ -33,6 +33,7 @@ import enterAnimation from "@/utils/enterAnimation";
 const outterContainer: Ref<HTMLElement | undefined> = ref()
 const innerContainer: Ref<HTMLElement | undefined> = ref()
 const intro: Ref<HTMLElement | undefined> = ref()
+const img: Ref<HTMLElement | undefined> = ref()
 
 const outterContainerAnimationConfig: anime.AnimeParams = {
     delay: 200,
@@ -49,21 +50,29 @@ const innerContainerAnimationConfig: anime.AnimeParams = {
     easing: 'easeOutExpo'
 }
 
+const imgAnimationConfig: anime.AnimeParams = {
+    delay: 1000,
+    opacity: [0, 1],
+    translateY: [50, 0]
+}
+
 onMounted(() => {
     enterAnimation(outterContainer.value!, outterContainerAnimationConfig);
     enterAnimation(innerContainer.value!, innerContainerAnimationConfig)
+    enterAnimation(img.value!, imgAnimationConfig)
 
-    intro.value!.childNodes.forEach((el, index) => {
-        if (!el.textContent) return;
+    for (let i = 0; i < intro.value!.childNodes.length; i++) {
+        const el = intro.value!.childNodes[i]
+        if (!el.textContent) continue;
 
         const elAnimationConfig: anime.AnimeParams = {
-            delay: 700 + 100 * index,
+            delay: 700 + 100 * i,
             opacity: [0, 1],
-            translateY: [-100, 0]
+            translateY: [-30, 0]
         }
 
         enterAnimation(el as HTMLElement, elAnimationConfig)
-    });
+    }
 })
 </script>
   
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -19,28 +19,15 @@
 </template>
   
 <script setup lang='ts'>
-// import Header from "./Header/index.vue"
-// import Top from "./Top/index.vue"
-// import Information from "./Infomation/index.vue"
-// import Profile from "./Profile/index.vue"
-// import Youtube from "./Youtube/index.vue"
-// import Discography from "./Discography/index.vue"
-// import Sns from "./Sns/index.vue"
-// import Contact from "./Contact/index.vue"
-// import Footer from "./Footer/index.vue"
-
-
-import { defineAsyncComponent } from "vue"
-
-const Header = defineAsyncComponent(() => import("./Header/index.vue"))
-const Top = defineAsyncComponent(() => import("./Top/index.vue"))
-const Information = defineAsyncComponent(() => import("./Infomation/index.vue"))
-const Profile = defineAsyncComponent(() => import("./Profile/index.vue"))
-const Youtube = defineAsyncComponent(() => import("./Youtube/index.vue"))
-const Discography = defineAsyncComponent(() => import("./Discography/index.vue"))
-const Sns = defineAsyncComponent(() => import("./Sns/index.vue"))
-const Contact = defineAsyncComponent(() => import("./Contact/index.vue"))
-const Footer = defineAsyncComponent(() => import("./Footer/index.vue"))
+import Header from "./Header/index.vue"
+import Top from "./Top/index.vue"
+import Information from "./Infomation/index.vue"
+import Profile from "./Profile/index.vue"
+import Youtube from "./Youtube/index.vue"
+import Discography from "./Discography/index.vue"
+import Sns from "./Sns/index.vue"
+import Contact from "./Contact/index.vue"
+import Footer from "./Footer/index.vue"
 </script>
   
 <style></style>
