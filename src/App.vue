5dc61b26da9fb424b7660ae739699292a9434a05
BSJJC <805086021@qq.com>
2023-10-06 16:28:54 +0800

    temp1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <el-scrollbar height="100vh">
-    <div id="body">
-      <Home></Home>
-    </div>
-  </el-scrollbar>
+    <el-scrollbar height="100vh">
+        <div id="body">
+            <Home></Home>
+        </div>
+    </el-scrollbar>
 </template>
 
 <script setup lang="ts">
@@ -12,10 +12,10 @@ import Home from "./views/home/index.vue";
 
 <style>
 #body {
-  background: #fff8a9 url(./assets/imgs/body-bg.png) center top/100% auto;
+    background: #fff8a9 url(./assets/imgs/body-bg.png) center top/100% auto;
 }
 
 .el-scrollbar__wrap {
-  scroll-behavior: smooth;
+    scroll-behavior: smooth;
 }
 </style>
