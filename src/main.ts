9920fe7c0697c05aabdd1594baf73c2e033c6e74
BSJJC <805086021@qq.com>
2023-10-06 16:38:52 +0800

    temp2

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import './css/index.css'
-import './css/style.css'
-import './css/fonts.css'
+import './css/index.css';
+import './css/style.css';
+import './css/fonts.css';
 
-const app = createApp(App)
+//test
 
-app.mount('#app')
+const app = createApp(App);
+
+app.mount('#app');
