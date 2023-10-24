25af228edea9ec0b4f1657454eacf9b029ce0a1a
BSJJC <805086021@qq.com>
2023-10-24 23:32:07 +0800

    feat: informations loading

--- src/views/information/Informations/components/informations-informations.vue
+++ src/views/information/Informations/components/informations-informations.vue
@@ -1,34 +1,45 @@
 <template>
-  <ul class="mt-10">
-    <li v-for="(i, index) in informations" :key="index" class="flex mb-4 min-w-[300px] w-[70%] m-auto md:mb-8">
-      <!-- informations pubulish date -->
+  <div>
+    <Transition mode="out-in">
       <div
-        class="fredoka-one flex justify-center items-center w-[30%] max-w-[140px] border-[#f60] border-x-[1px] text-[#f60] font-bold"
+        v-if="!informations"
+        class="fredoka-one loading-animation w-[70%] h-[300px] m-auto flex justify-center items-start mt-10 pt-20 text-[2rem] font-bold text-[#f70] md:text-[3rem]"
       >
-        <div class="flex justify-center items-center flex-col text-[0.7rem]">
-          <span class="w-full text-right leading-3 md:text-[1rem] md:leading-4">
-            <!-- year -->
-            {{ i.date.toString().substring(0, 4) }}
-            <br />
-            <!-- month -->
-            {{ i.date.toString().substring(4, 6) }}
-          </span>
-        </div>
-
-        <span class="text-[2rem] md:text-[2.5rem]">
-          <!-- day -->
-          {{ i.date.toString().substring(6, 8) }}
-        </span>
+        Loading...
       </div>
 
-      <!-- news title -->
-      <div
-        class="m-plus-rounded-1c flex justify-start items-center w-[70%] border-r-[1px] border-[#f60] px-3 md:text-[1.3rem] md:w-full"
-      >
-        <a href="" class="text-[#3865e0] font-black">{{ i.title }}</a>
-      </div>
-    </li>
-  </ul>
+      <ul v-else class="mt-10">
+        <li v-for="(i, index) in informations" :key="index" class="flex mb-4 min-w-[300px] w-[70%] m-auto md:mb-8">
+          <!-- informations pubulish date -->
+          <div
+            class="fredoka-one flex justify-center items-center w-[30%] max-w-[140px] border-[#f60] border-x-[1px] text-[#f60] font-bold"
+          >
+            <div class="flex justify-center items-center flex-col text-[0.7rem]">
+              <span class="w-full text-right leading-3 md:text-[1rem] md:leading-4">
+                <!-- year -->
+                {{ i.date.toString().substring(0, 4) }}
+                <br />
+                <!-- month -->
+                {{ i.date.toString().substring(4, 6) }}
+              </span>
+            </div>
+
+            <span class="text-[2rem] md:text-[2.5rem]">
+              <!-- day -->
+              {{ i.date.toString().substring(6, 8) }}
+            </span>
+          </div>
+
+          <!-- news title -->
+          <div
+            class="m-plus-rounded-1c flex justify-start items-center w-[70%] border-r-[1px] border-[#f60] px-3 md:text-[1.3rem] md:w-full"
+          >
+            <a href="" class="text-[#3865e0] font-black">{{ i.title }}</a>
+          </div>
+        </li>
+      </ul>
+    </Transition>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -40,11 +51,39 @@ type TInformation = {
   title: string;
 };
 
-const informations: Ref<TInformation[]> = ref([]);
+const informations: Ref<TInformation[] | null> = ref(null);
 
 onBeforeMount(async () => {
   informations.value = await getInformation();
 });
 </script>
 
-<style></style>
+<style scoped>
+.loading-animation {
+  animation: bounce 1s infinite;
+}
+
+@keyframes bounce {
+  0%,
+  100% {
+    transform: translateY(-20px);
+    animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
+  }
+
+  50% {
+    transform: translateY(0);
+    animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+  }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+</style>
