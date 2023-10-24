ca268c0bb9305401c77127bd8e61a0d8966e260d
BSJJC <805086021@qq.com>
2023-10-24 16:50:02 +0800

    feat: style

--- src/views/information/Informations/components/informations-informations.vue
+++ src/views/information/Informations/components/informations-informations.vue
@@ -1,15 +1,11 @@
 <template>
-  <ul>
-    <li
-      v-for="(i, index) in informations"
-      :key="index"
-      class="flex justify-center items-center w-[97%] mx-3 mb-4 md:w-[90%]"
-    >
-      <!-- all informations -->
+  <ul class="mt-10">
+    <li v-for="(i, index) in informations" :key="index" class="flex mb-4 min-w-[300px] w-[90%] m-auto">
+      <!-- informations pubulish date -->
       <div
-        class="fredoka-one flex border-x-[1px] border-[#f60] justify-center items-center w-[30%] text-[#f60] font-bold md:w-[15%]"
+        class="fredoka-one flex justify-center items-center w-[30%] max-w-[140px] border-[#f60] border-x-[1px] text-[#f60] font-bold md:w-[15%]"
       >
-        <div class="flex justify-center items-center flex-col text-[0.5rem] pr-2 md:text-[0.8rem]">
+        <div class="flex justify-center items-center flex-col text-[0.7rem] md:text-[0.8rem]">
           <span class="w-full text-right leading-3">
             <!-- year -->
             {{ i.date.toString().substring(0, 4) }}
@@ -19,17 +15,15 @@
           </span>
         </div>
 
-        <div>
-          <span class="text-[2.5rem]">
-            <!-- day -->
-            {{ i.date.toString().substring(6, 8) }}
-          </span>
-        </div>
+        <span class="text-[2rem]">
+          <!-- day -->
+          {{ i.date.toString().substring(6, 8) }}
+        </span>
       </div>
 
       <!-- news title -->
       <div class="m-plus-rounded-1c w-[70%] border-r-[1px] border-[#f60] px-3 md:w-[85%]">
-        <a href="" class="text-[#3865e0] font-bold">{{ i.title }}</a>
+        <a href="" class="text-[#3865e0] font-black">{{ i.title }}</a>
       </div>
     </li>
   </ul>
