a870e945afdbc7944640844dc343e0c4ebcb6561
BSJJC <805086021@qq.com>
2023-10-26 20:53:43 +0800

    chore

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-  //   You cannot use the `You cannot use the `createWebHashHistory` mode, otherwise, when you want to scroll to a specific section using the header's navigation, the page will be redirected.` mode, otherwise, when you want to scroll to a specific section using the header's navigation, the page will be redirected.
+  //Don't use the `createWebHashHistory` mode, otherwise, when you want to scroll to a specific section using the Header's navigation, the page will be redirected.
   history: createWebHistory(),
   routes: [
     {
