5f35c24bb17e9f89ef8e6e5ea19c291de60b7f27
BSJJC <805086021@qq.com>
2023-10-31 01:59:47 +0800

    chore

--- src/utils/toggleToTopVisible.ts
+++ src/utils/toggleToTopVisible.ts
@@ -1,22 +1,31 @@
 import { storeToRefs } from 'pinia';
 import { useFooter } from '@/stores/footerStore';
 
+// Define the structure of the scroll position options for the element
 type TElScrollOption = {
-  scrollLeft: number;
-  scrollTop: number;
+  scrollLeft: number; // Horizontal scroll position
+  scrollTop: number; // Vertical scroll position
 };
 
 /**
- * Switch whether to display the atTop button
- * @param {TElScrollOption} elOptions -Parameters that match the ElScrollOption type
+ * Toggle the visibility of the 'to top' button based on scroll position.
+ * @param {TElScrollOption} elOptions - Object containing scroll position properties (scrollLeft, scrollTop).
  */
 export default function toggleToTopVisible(elOptions: TElScrollOption): void {
+  // Extract the vertical scroll position from the provided options
   const top = elOptions.scrollTop;
 
   const footerStore = useFooter();
+
   const { atTop } = storeToRefs(footerStore);
 
-  // Show the top button when scrolling over 500
-  if (top >= 500) atTop.value = false;
-  if (top === 0) atTop.value = true;
+  // Show the 'to top' button when scrolling over 500 pixels from the top
+  if (top >= 500) {
+    atTop.value = false; // If scrolled more than 500 pixels, the 'to top' button is visible.
+  }
+
+  // Hide the 'to top' button when scrolled back to the top of the page (at 0 pixels)
+  if (top === 0) {
+    atTop.value = true; // If scrolled back to the top, the 'to top' button is hidden.
+  }
 }
