2b3d975e29319ad5af6b5eeb694f0a46402160b3
BSJJC <805086021@qq.com>
2023-11-02 23:46:52 +0800

    'chore'

--- src/views/information/AllInformations/informations/20200913-003/20200913-003.vue
+++ src/views/information/AllInformations/informations/20200913-003/20200913-003.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="min-h-screen">02</div>
+  <div class="info-container">
+    <div class="info-date">2020.09.13</div>
+
+    <h1 class="h1-title">P丸様。のすべてがわかる！待望のファンブック！！ 「ぴーまるぶっく！。」発売決定！</h1>
+
+    <table>
+      <tr>
+        <td>
+          <img src="./img/20200913003-01.jpg" alt="ぴーまるぶっく！ image" />
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style></style>
+<style scoped>
+@import url('@/css/information.css');
+</style>
