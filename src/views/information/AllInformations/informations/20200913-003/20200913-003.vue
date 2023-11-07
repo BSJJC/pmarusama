9fc8714c1d0c0f27a142d97d9655e6cc54f5a948
BSJJC <805086021@qq.com>
2023-11-07 23:58:05 +0800

    chore

--- src/views/information/AllInformations/informations/20200913-003/20200913-003.vue
+++ src/views/information/AllInformations/informations/20200913-003/20200913-003.vue
@@ -60,7 +60,7 @@
         <div class="w-[98%] m-auto rounded-lg bg-[#f60] grid grid-cols-2 p-2 md:grid-cols-3 lg:grid-cols-4">
           <div v-for="(i, index) in shopImages" :key="index" class="flex justify-center items-center p-1">
             <a :href="i.url" target="_blank">
-              <img :src="i.src" :alt="i.alt" class="w-full" />
+              <img :src="i.src" :alt="i.alt" class="w-full transition-all duration-300 ease-in-out hover:scale-110" />
             </a>
           </div>
         </div>
