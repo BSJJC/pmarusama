e6a2f1ee7ce3a0a3eafa89d4848dcf5526f0c29e
BSJJC <805086021@qq.com>
2023-11-08 07:40:29 +0800

    feat: 20210814001

---
+++ src/views/information/AllInformations/informations/20210814-001/20210814-001.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="info-container">
+    <span class="info-date">2021.08.14</span>
+
+    <h1 class="h1-title">新曲「ときめきブローカー」MV公開！デジタル配信スタート！</h1>
+
+    <p>
+      <img src="./img/20210814001-01.jpg" alt="tokimeki cover" />
+    </p>
+
+    <p>
+      3月に発売された1stアルバム『Sunny!!』に収録された「シル・ヴ・プレジデント」が大ヒット中！MVはYouTubeで3,600万回視聴を超え、TikTok人気楽曲ランキング連続首位を獲得し、現在もチャート上位にランクイン中！
+    </p>
+
+    <p>
+      さらに英語、中国語で歌唱したショートバージョンも好評な中、待望の新曲「ときめきブローカー」のMVが本日公開された。「シル・ヴ・プレジデント」のクリエイターたちが集結し、P丸様。の変幻自在でキュートな声、アップテンポでポップなアレンジと思わず口ずさんでしまう癖になる歌詞の世界観を感じられるMVとなっています！
+    </p>
+
+    <p>
+      デジタル配信も本日同時スタート！TikTokでの配信も8/15(日)順次スタート予定となっており、新たなP丸様。現象が期待される。
+    </p>
+
+    <h2 class="h2-title">【MV】ときめきブローカー／P丸様。</h2>
+
+    <div class="flex flex-col space-y-8 md:flex-row md:space-x-8">
+      <div class="md:w-[30%]">
+        <img src="./img/20210814001-02.jpg" alt="tokimeko small cover" />
+      </div>
+
+      <div>
+        <table>
+          <tr>
+            <td>MV URL：</td>
+            <td>
+              <a href="https://www.youtube.com/watch?v=paoXdQREpYM" target="_blank">https://youtu.be/paoXdQREpYM</a>
+            </td>
+          </tr>
+          <tr>
+            <td>Words & Arrange：</td>
+            <td>ナナホシ管弦楽団</td>
+          </tr>
+          <tr>
+            <td>Music：</td>
+            <td>岩見 陸</td>
+          </tr>
+          <tr>
+            <td>Illust：</td>
+            <td>nanao</td>
+          </tr>
+          <tr>
+            <td>Movie：</td>
+            <td>えるいー</td>
+          </tr>
+          <tr>
+            <td>Art Direction：</td>
+            <td>P丸様。</td>
+          </tr>
+          <tr>
+            <td>デジタル配信URL：</td>
+            <td>
+              <a href="https://stpr.lnk.to/Pmarusama_TokimekiBroker" target="_blank">
+                https://stpr.lnk.to/Pmarusama_TokimekiBroker
+              </a>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
