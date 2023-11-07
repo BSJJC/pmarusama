3956e76279c492c894485931eee1eea2ce038bd4
BSJJC <805086021@qq.com>
2023-11-08 06:32:23 +0800

    feat: 20210217001

---
+++ src/views/information/AllInformations/informations/20210217-001/20210217-001.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="info-container">
+    <span class="info-date">2021.02.17</span>
+
+    <h1 class="h1-title">P丸様。オリジナル「Tカード」発行決定！</h1>
+
+    <p>
+      <img src="./img/20210217001-01.jpg" alt="T card image" />
+    </p>
+
+    <p>
+      1stフルアルバム「Sunny!!」のリリースを記念して、「Tカード(P丸様。)」を、3月16日(火)より全国のTSUTAYAと旭屋書店にて発行開始いたします。
+      <br />
+      また、2月17日(水)12：00より「Tカード(P丸様。)」公式サイトにて事前発行受付を開始いたします。
+    </p>
+
+    <h2 class="h2-title">「Tカード(P丸様。)」公式サイト</h2>
+
+    <p>
+      <a href="https://tfan.jp/pmarusama/" target="_blank">https://tfan.jp/pmarusama/</a>
+      <br />
+      「Tカード(P丸様。)」の情報や特典の申し込みなどを随時更新してまいります。
+    </p>
+
+    <ul>
+      <li>
+        <strong>カード発行手数料：500円(税抜)</strong>
+      </li>
+      <li>
+        <ul class="ml-10">
+          <li>なくなり次第、発行終了になります。</li>
+          <li>TSUTAYAレンタル利用登録料が別途かかる場合もございます。</li>
+          <li>ネット注文受付は別途配送手数料がかかります。</li>
+        </ul>
+      </li>
+    </ul>
+
+    <h2 class="h2-title">「Tカード(P丸様。)」の申し込み方法、発行期間など</h2>
+
+    <ul>
+      <li>
+        <strong>全国のTSUTAYA店頭発行期間：2021年3月16日(火)～</strong>
+      </li>
+      <li class="ml-10">
+        申込店舗は
+        <a href="https://tfansite.jp/guide/storelist">こちら</a>
+      </li>
+    </ul>
+
+    <ul>
+      <li>
+        <strong>全国の旭屋書店店頭発行期間：2021年3月16日(火)～</strong>
+      </li>
+      <li class="ml-10">
+        申込店舗は
+        <a href="https://tfansite.jp/guide/asahiya_storelist">こちら</a>
+      </li>
+      <li class="ml-10">一部店舗を除きます。</li>
+    </ul>
+
+    <ul>
+      <li>
+        <strong>ネット注文受付＆発行期間：2021年2月17日(水)12:00～</strong>
+      </li>
+      <li class="ml-10">2021年3月15日(月)までにお申し込みの方は、2021年3月16日(火)以降のお届けとなります。</li>
+    </ul>
+
+    <h2 class="h2-title">
+      モバイルTカードでも表示Tカード(P丸様。)はモバイルTカード(スマートフォンアプリ)でも表示されます。
+    </h2>
+
+    <p>
+      表示方法はこちらのページをご確認ください。
+      <br />
+      <a href="https://tfan.jp/mobat">https://tfan.jp/mobat</a>
+    </p>
+
+    <p>Tカード(キャラクターデザイン)のモバイルTカードでの表示は、予告なく終了することがあります。</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
