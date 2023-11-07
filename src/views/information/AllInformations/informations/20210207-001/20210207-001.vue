f3ee2c4341ecfba61c159bd0556c41ff7973f1be
BSJJC <805086021@qq.com>
2023-11-08 04:55:58 +0800

    feat: 20210207001

---
+++ src/views/information/AllInformations/informations/20210207-001/20210207-001.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="info-container">
+    <span class="info-date">2021.02.07</span>
+
+    <h1 class="h1-title">大好評のオフィシャルファンブック「ぴーまるぶっく！。」重版決定！</h1>
+
+    <p class="img-center">
+      <img src="./img/20200913001-01.jpg" alt="sunny image" />
+    </p>
+
+    <p>
+      1stフルアルバム「Sunny!!」の発売を記念して、昨年9月に発売された、”P 丸様
+      。”のすべてがわかるオフィシャルファンブック「ぴーまるぶっく！。」の重版が決定いたしました。
+    </p>
+
+    <p>
+      発表と同時にご予約開始から、すでに、楽天ブックス “本”日別ランキング1位(9 月 8 日付)、 Amazonランキング
+      タレント本(総合)(9月9日付)、若者文化、サブカルチャー、漫画・アニメ・BL(イラスト集・オフィシャルブックイラスト集・オフィシャルブック)(9月
+      10日付)キャラクター部門(9月 9,10日付)等で1位になった注目の本作が、アルバムのリリースにあわせて重版されます！
+    </p>
+
+    <div>
+      <p class="font-bold">【商品概要】</p>
+      <table>
+        <tr>
+          <td>■品名：</td>
+          <td>ぴーまるぶっく！。</td>
+        </tr>
+        <tr>
+          <td>■価格：</td>
+          <td>1,800 円(税抜)</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
