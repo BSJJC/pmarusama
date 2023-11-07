0b6eb76c365b7ad71cf8cfd9cfbb863d2fb5a1ca
BSJJC <805086021@qq.com>
2023-11-08 06:41:27 +0800

    feat: 20210306001

---
+++ src/views/information/AllInformations/informations/20210306-001/20210306-001.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="info-container">
+    <span class="info-date">2021.03.06</span>
+
+    <h1 class="h1-title">「ゆるふわ～」新規オリジナルグッズ発売決定！</h1>
+
+    <p>
+      <img src="./img/20210306001-01.jpg" alt="content informaton image" />
+    </p>
+
+    <p>
+      1stフルアルバム「Sunny!!」の発売を記念して、「ゆるふわ～」な新商品が登場します!
+      <br />
+      P☻Shop(ぴーまるしょっぷ)の他、アニメイトやヴィレッジヴァンガード(いずれも一部店舗)の店舗や、それぞれのオンラインショップでもお買い求めいただけますので、お見逃しなく!
+    </p>
+
+    <table>
+      <tr>
+        <td>◎</td>
+        <td>ゆるふわ～うさぎさんパスケース!。</td>
+        <td>価格:1,800円(税込)</td>
+      </tr>
+      <tr>
+        <td>◎</td>
+        <td>ゆるふわ～くまくんパスケース!</td>
+        <td>価格:1,800円(税込)</td>
+      </tr>
+      <tr>
+        <td>◎</td>
+        <td>ゆるふわ～ぷくぷくしーる!</td>
+        <td>価格:550円(税込)</td>
+      </tr>
+    </table>
+
+    <h2 class="h2-title">【販売箇所】</h2>
+
+    <p>
+      P☻Shop(ぴーまるしょっぷ)
+      <br />
+      ヴィレッジヴァンガード(一部店舗)、
+      <br />
+      ヴィレッジヴァンガードオンラインストア
+      <br />
+      アニメイト(一部店舗)、アニメイト通販
+    </p>
+
+    <h2 class="h2-title">【P☻Shop(ぴーまるしょっぷ)店舗情報】</h2>
+
+    <table>
+      <tr>
+        <td>開催期間：</td>
+        <td>2021年3月31日(水)まで</td>
+      </tr>
+      <tr>
+        <td>住所：</td>
+        <td>
+          池袋サンシャインシティALTA
+          <br />
+          東京都豊島区東池袋3丁目1-3
+          <br />
+          サンシャインシティ
+          <br />
+          ワールドインポートマートビル1F
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
