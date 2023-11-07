42c570948bcd181d582acc0e2b3ab107aa767323
BSJJC <805086021@qq.com>
2023-11-08 07:27:38 +0800

    feat: 20210807001

---
+++ src/views/information/AllInformations/informations/20210807-001/20210807-001.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="info-container">
+    <span class="info-date">2021.08.07</span>
+
+    <h1 class="h1-title">「シル・ヴ・プレジデント」英語版・中国語版を公開！</h1>
+
+    <p>
+      3月に発売された1stアルバム『Sunny!!』に収録された「シル・ヴ・プレジデント」が大ヒット中!
+      <br />
+      MVはYouTubeで3,400万回視聴を超え、TikTok人気楽曲ランキング連続首位を獲得し現在もチャート上位にランクイン中!
+      <br />
+      そんな楽曲をより多くの人にもっと楽しんで欲しいと今回「シル・ヴ・プレジデント」ショートver.を英語、中国語で歌唱したバージョンを自身のYouTubeにて公開!
+    </p>
+
+    <ul>
+      <li>
+        <a href="https://www.youtube.com/watch?v=jYemyJPS4rU" target="_blank">
+          Sʼil vous President(シル・ヴ・プレジデント English Ver.)／P丸様。
+        </a>
+      </li>
+      <li>
+        <a href="https://www.youtube.com/watch?v=sCb2Ug5ig7Y" target="_blank">
+          如果我成为大统领(シル・ヴ・プレジデント Chinese Ver.)／P丸様。
+        </a>
+      </li>
+    </ul>
+
+    <p>TikTokでの配信も8月8日(日)0時よりスタート!</p>
+
+    <ul>
+      <li>
+        <a href="https://m.tiktok.com/h5/share/music/6992093019763640322.html" target="_blank">
+          Sʼil vous President(シル・ヴ・プレジデント English Ver.)
+        </a>
+      </li>
+      <li>
+        <a href="https://m.tiktok.com/h5/share/music/6992093019818985473.html" target="_blank">
+          如果我成为大统领(シル・ヴ・プレジデント Chinese Ver.)
+        </a>
+      </li>
+    </ul>
+
+    <p class="mt-8">
+      さらに「P丸様。×ナナホシ管弦楽団」での新曲が来週公開されることが決定!
+      <br />
+      お楽しみに!!
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
