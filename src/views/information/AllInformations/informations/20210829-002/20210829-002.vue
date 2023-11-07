0caa2eea7ca33718ad5f6111de6693a9f0accfc5
BSJJC <805086021@qq.com>
2023-11-08 07:51:02 +0800

    feat: 20210829002

---
+++ src/views/information/AllInformations/informations/20210829-002/20210829-002.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="info-container">
+    <span class="info-date">2021.08.29</span>
+
+    <h1 class="h1-title">「とっても大好きっ！」MV公開！</h1>
+
+    <p>
+      <img src="./img/20210829002-01.jpg" alt="tottemosuki cover" />
+    </p>
+
+    <p>
+      「とっても大好きっ！」は1stアルバム『Sunny!!』に収録された楽曲で、神聖かまってちゃんのの子による詞とメロディー、えいりな刃物による映像によって表現されるシニカルな世界と、P丸様。の変幻自在でキュートな声が相まって夏の終わりにぴったりな、ちょっぴり切ないMVとなっています。
+    </p>
+
+    <h2 class="h2-title">【MV】とっても大好きっ！／P丸様。</h2>
+
+    <div class="flex flex-col space-y-6 md:flex-row md:space-x-6">
+      <div class="md:w-[30%]">
+        <img src="./img/20210829002-02.jpg" alt="discograph cover" />
+      </div>
+
+      <div>
+        <table>
+          <tr>
+            <td>MV URL：</td>
+            <td>
+              <a href="https://www.youtube.com/watch?v=Xzdqw_L4ZUk" target="_blank">https://youtu.be/Xzdqw_L4ZUk</a>
+            </td>
+          </tr>
+          <tr>
+            <td>Words & Music：</td>
+            <td>の子</td>
+          </tr>
+          <tr>
+            <td>Arrange：</td>
+            <td>木村篤史 (glasswerks)</td>
+          </tr>
+          <tr>
+            <td>Movie：</td>
+            <td>えいりな刃物</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
