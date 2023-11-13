448477aa4a0e5f022fe04c8a254ef6ac38fc32fd
BSJJC <805086021@qq.com>
2023-11-14 03:33:18 +0800

    feat: 20220605001

---
+++ src/views/information/AllInformations/informations/20220605-001/20220605-001.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="info-container">
+    <h1 class="h1-title">ゲームセンターのUFOキャッチャーにP丸様。の景品が初登場！</h1>
+
+    <p>
+      <img src="./img/20220605001-01.jpg" alt="content info" />
+    </p>
+
+    <p>
+      7月下旬、全国のゲームセンターのUFOキャッチャーにP丸様。のプライズが初登場します！7月から12月にかけて、P丸様。・くまくん・うさぎさんのぬいぐるみやマスコット等が続々登場予定です。お楽しみに！
+    </p>
+
+    <p>
+      第1弾の取扱店舗情報は7月上旬に
+      <a href="https://segaplaza.jp/" target="_blank">セガプラザ</a>
+      内に掲載予定です。
+      <br />
+      ぜひチェックしてみてくださいね！
+    </p>
+
+    <p>
+      <a href="https://segaplaza.jp/" target="_blank">セガプラザ</a>
+      <br />
+      <a href="https://twitter.com/seganewsnavi" target="_blank">セガプラザ公式Twitter</a>
+    </p>
+
+    <p>
+      <small>※UFO キャッチャー、UFO CATCHER は株式会社セガの登録商標または商標です。</small>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
