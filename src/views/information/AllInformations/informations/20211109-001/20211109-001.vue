c11dd01438a19283beae4e836120716c9c47727d
BSJJC <805086021@qq.com>
2023-11-14 03:07:58 +0800

    feat: 20211109001

---
+++ src/views/information/AllInformations/informations/20211109-001/20211109-001.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="info-container">
+    <h1 class="h1-title">「TikTok流行語大賞2021」に『シル・ヴ・プレジデント』がノミネート！</h1>
+
+    <p>
+      <img src="./img/20211109001-01.jpg" alt="tiktok" />
+    </p>
+
+    <p>
+      <strong>
+        『シル・ヴ・プレジデント(大統領になったらね)』が「TikTok流行語大賞2021」にノミネートされました！
+        <br />
+        本日11月9日(火)より、TikTokアプリ内にて「TikTok流行語大賞2021」ユーザー投票が開始！
+      </strong>
+    </p>
+
+    <h2 class="h2-title">＜「TikTok流行語大賞2021」とは＞</h2>
+
+    <p>
+      TikTokで今年1年間に流行ったハッシュタグチャレンジや楽曲、コトバなどの流行語をノミネートし、2021年の動画投稿数、再生数、いいね数、TikTokアプリ上のユーザー投票数などをもとに総合的に判断し、今年最もTikTokで流行したモノやコトを決定する企画。
+    </p>
+
+    <ul>
+      <li>投票期間：11月9日(火)〜11月22日(月)</li>
+      <li>参加方法：特設サイトから1日3回ずつ毎日投票可能。</li>
+      <li>
+        特設サイト（投票受付ページ）：
+        <a
+          href="https://activity.tiktok.com/magic/eco/runtime/release/61810b48b8df1903293bb826?appType=tiktok"
+          target="_blank"
+        >
+          投票はこちらから！
+        </a>
+      </li>
+      <li>
+        特設サイト（ノミネートワード解説ページ）：
+        <a
+          href="https://activity.tiktok.com/magic/eco/runtime/release/61810c34bc54ab031c9452fd?appType=tiktok"
+          target="_blank"
+        >
+          知らないノミネートワードはこちらからチェック！
+        </a>
+      </li>
+      <li>
+        大賞発表：12月8日(水)にTikTokアプリ上で配信されるスペシャルLIVE番組「TikTok Creator’s Lab. 2021
+        Celebrations」にて発表
+      </li>
+    </ul>
+
+    <p>※大賞は、投票数などを元に、運営で総合的に判断し決定</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
