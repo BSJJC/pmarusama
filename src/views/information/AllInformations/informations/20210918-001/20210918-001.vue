522b86c59345d7129533a90071349ed471ac9d85
BSJJC <805086021@qq.com>
2023-11-14 02:38:30 +0800

    feat: 20210918001

---
+++ src/views/information/AllInformations/informations/20210918-001/20210918-001.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="info-container">
+    <h1 class="h1-title">P丸様。の3Dモデルが完成!!お披露目となる初ライブを自身の誕生日である9月30日(木)に開催!</h1>
+
+    <p>
+      <img src="./img/20210918001-01.jpg" alt="3d date" />
+    </p>
+
+    <p>
+      “P丸様。“が、遂に3Dで登場!!
+      <br />
+      今回、お披露目ライブとして、自身の誕生日である9月30日に初のライブを開催いたします。
+      <br />
+      “P丸様。“初となる今回のライブは、YouTubeのP丸様。公式チャンネルで、どなたでも無料でお楽しみいただけます。
+      <br />
+      さらにお披露目ライブで、重大発表も…!?ご期待ください!
+    </p>
+
+    <h2 class="h2-title">『P丸様。3D!! お披露目ライブ!』 概要</h2>
+
+    <table>
+      <tr>
+        <td>出演：</td>
+        <td>P丸様。</td>
+      </tr>
+      <tr>
+        <td>開演日時：</td>
+        <td>2021年9月30日(木) 19時〜</td>
+      </tr>
+      <tr>
+        <td>ライブ配信サイト：</td>
+        <td><a href="https://www.youtube.com/channel/UCLqCmbd6bgcLaBVz3aA-68A" target="_blank">YouTube P丸様。</a></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
