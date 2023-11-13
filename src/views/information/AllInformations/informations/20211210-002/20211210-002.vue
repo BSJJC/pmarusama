e7c2d5a50bc3918bb9b2af6ed0672b2daf22838c
BSJJC <805086021@qq.com>
2023-11-14 03:29:53 +0800

    feat: 20211210002

---
+++ src/views/information/AllInformations/informations/20211210-002/20211210-002.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="info-container">
+    <h1 class="h1-title">新曲「ちきゅう大爆発」MV公開＆SNSシェアキャンペーンスタート！</h1>
+
+    <p>
+      <img src="./img/20211210002-01.jpg" alt="chikyu boom" />
+    </p>
+
+    <p>
+      今作は作詞・作曲・編曲にピノキオピーを迎えた楽曲。ピノキオピーらしさ全開の楽曲とP丸様。のかわいらしいゆるふわ
+      感の歌声とマッチし、独特の世界観が生み出されている。「シル・ヴ・プレジデント」「ときめきブローカー」とはまた違
+      う変幻自在の歌声で、聴けば聴くほどクセになる楽曲となっている。
+      MVはディレクター・大野悟氏のもと、複数のクリエイターが集結し制作され、音楽と合わせてテンポよくシーンが変化し、
+      P丸様。の可愛いらしいダンスと表情が魅力的なMVとなっている。
+    </p>
+
+    <h2 class="h2-title">【MV】ちきゅう大爆発/P丸様。</h2>
+
+    <table>
+      <tr>
+        <td>【MV】ちきゅう大爆発/P丸様。</td>
+        <td>
+          <a href="https://youtu.be/8fpky0u3vVc" target="_blank">https://youtu.be/8fpky0u3vVc</a>
+        </td>
+      </tr>
+      <tr>
+        <td>Words & Music & Arrangement :</td>
+        <td>ピノキオピー</td>
+      </tr>
+      <tr>
+        <td>Director :</td>
+        <td>Satoru Ohno (THINKR)</td>
+      </tr>
+      <tr>
+        <td>Technical Director / 3D Λrtist :</td>
+        <td>F A T I M A</td>
+      </tr>
+      <tr>
+        <td>Visual Creator :</td>
+        <td>
+          Hironobu Nagasawa / pm21 / Satoru Ohno / TECH. ReeeznD / いがきち / こんぶ / すもるめ / のんたん /はちの
+        </td>
+      </tr>
+      <tr>
+        <td>Illustration (pm21 Part) :</td>
+        <td>Koharu Yamashita</td>
+      </tr>
+      <tr>
+        <td>Production Manager :</td>
+        <td>Activ8 Inc.</td>
+      </tr>
+      <tr>
+        <td>デジタル配信URL :</td>
+        <td>
+          <a href="https://stpr.lnk.to/Pmarusama_ThePlanetBigBang" target="_blank">
+            https://stpr.lnk.to/Pmarusama_ThePlanetBigBang
+          </a>
+        </td>
+      </tr>
+    </table>
+
+    <p>
+      ※TikTok配信は12/11(土)0時〜開始!
+      <br />
+      <a href="https://m.tiktok.com/h5/share/music/7039225813291501569.html" target="_blank">
+        https://m.tiktok.com/h5/share/music/7039225813291501569.html
+      </a>
+    </p>
+
+    <p>
+      <img src="./img/20211210002-02.jpg" alt="sns guide" />
+    </p>
+
+    <p>
+      デジタル配信リリースを記念してSNSシェアキャンペーンもスタート。「ちきゅう大爆発」を再生してSNSで共有/シ
+      ェアして応募すると抽選で10名様に「P丸様。ふるぐらふぃっくTしゃつ!Peace Parade ver.」が当たるというものだ。
+      また応募者全員には「ちきゅう大爆発」のオリジナルスマホ壁紙がプレゼントされる。
+      TikTok配信は12月11日(土)0時から開始されるので、「シル・ヴ・プレジデント」「ときめきブローカー」など投稿され
+      ている多くの楽曲と同様にぜひ「ちきゅう大爆発」でも楽しい動画のシェアを楽しんで欲しい。
+    </p>
+
+    <p>
+      応募フォーム:
+      <a href="https://questant.jp/q/5QSBO6BE" target="_blank">https://questant.jp/q/5QSBO6BE</a>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
