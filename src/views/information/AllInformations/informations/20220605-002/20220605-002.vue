bf693ff55e97f236c4ef0f6d808729504dba7241
BSJJC <805086021@qq.com>
2023-11-14 03:36:45 +0800

    feat: 20220605002

---
+++ src/views/information/AllInformations/informations/20220605-002/20220605-002.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="info-container">
+    <h1 class="h1-title">P丸様。初のガチャガチャが発売決定！</h1>
+
+    <p>
+      <img src="./img/20220801002-01.jpg" alt="gachagacha image" />
+    </p>
+
+    <p>
+      P丸様。初のカプセルトイが発売決定いたしました！
+      <br />
+      デフォルメイラストを使用したラバーキーホルダーと缶バッジが登場します。
+    </p>
+
+    <p>
+      取扱店舗は8月上旬より店頭に並び次第、下記商品ページ内に順次反映予定です。
+      <br />
+      ぜひチェックしてみてくださいね！お楽しみに！
+    </p>
+
+    <p>
+      <a href="https://www.takaratomy-arts.co.jp/items/item.html?n=Y059926" target="_blank">
+        でふぉラバ！P丸様。キーホルダー 商品ページ
+      </a>
+      <br />
+      <a href="https://www.takaratomy-arts.co.jp/items/item.html?n=Y063763" target="_blank">
+        P丸様。デフォメ缶バッジ 商品ページ
+      </a>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
