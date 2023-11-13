292f09330d841347482941aee50856d1c1cbfd2e
BSJJC <805086021@qq.com>
2023-11-14 03:18:16 +0800

    feat: 20211201001

---
+++ src/views/information/AllInformations/informations/20211201-001/20211201-001.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="info-container">
+    <h1 class="h1-title">
+      『シル・ヴ・プレジデント』がスマートフォンアプリ「太鼓の達人プラス★新曲取り放題」で配信スタート！
+    </h1>
+
+    <p>
+      <img src="./img/20211201001-01.jpg" alt="Taiko no Tatsujin" />
+    </p>
+
+    <p>
+      <strong>ぜひ遊んでみてくださいね。</strong>
+    </p>
+
+    <a href="https://app.taiko-ch.net/" target="_blankhttps://app.taiko-ch.net/">
+      スマートフォンアプリ「太鼓の達人プラス★新曲取り放題」公式サイト
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
