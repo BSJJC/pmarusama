05436b6306dc4e1b0eeeba3d87df31cd40aa07a0
BSJJC <805086021@qq.com>
2023-11-14 03:03:38 +0800

    feat: 20211026001

--- src/autoComponents/components.d.ts
+++ src/autoComponents/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    ElButton: typeof import('element-plus/es')['ElButton']
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
