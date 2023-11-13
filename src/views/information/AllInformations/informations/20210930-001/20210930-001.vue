3fed426a7d71c6421ceb59aea93d6ec711e34992
BSJJC <805086021@qq.com>
2023-11-14 02:41:53 +0800

    feat: 20210930001

---
+++ src/views/information/AllInformations/informations/20210930-001/20210930-001.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="info-container">
+    <h1 class="h1-title">初のワンマンライブ！両国国技館バーチャルライブが開催決定！！</h1>
+
+    <p>
+      <img src="./img/20210930001-01.jpg" alt="live date" />
+    </p>
+
+    <table>
+      <tr>
+        <td>会場：</td>
+        <td>両国国技館</td>
+      </tr>
+      <tr>
+        <td>開催日時：</td>
+        <td>2021年12月11日 & 12日</td>
+      </tr>
+    </table>
+
+    <h2 class="h2-title">チケット発売についての詳細は後日発表！</h2>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
