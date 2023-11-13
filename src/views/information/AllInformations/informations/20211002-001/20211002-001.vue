6f89d51cafa8d6e588150d6c464c59bf23bc8127
BSJJC <805086021@qq.com>
2023-11-14 02:54:24 +0800

    feat: 20211002001

---
+++ src/views/information/AllInformations/informations/20211002-001/20211002-001.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="info-container">
+    <h1 class="h1-title">初ワンマンライブ！バーチャルライブ「Peace Parade!!」in両国国技館、チケット受付スタート！</h1>
+
+    <p>
+      P丸様。の初となるワンマンライブ、両国国技館バーチャルライブ「Peace
+      Parade!!」が12月11日(土)、12日(日)と2days開催、チケット受付が本日10月2日18:00よりスタートします。
+      <br />
+      小中学生のリスナーも多いということでファミリー席も用意されており、初めてのライブという方もご家族と一緒に安心して楽しめるようになっています。
+      <br />
+      先行受付期間は10月17日(日)23:59までとなっているので、是非チェックしてみてください︕
+    </p>
+
+    <h2 class="h2-title">＜開催日時＞</h2>
+
+    <p>
+      2021年12月11日(土) OPEN 16:00 / START 17:00
+      <br />
+      2021年12月12日(日) OPEN 15:00 / START 16:00
+    </p>
+
+    <h2 class="he-title">＜会場＞</h2>
+
+    <p>両国国技館</p>
+
+    <h2 class="h2-title">＜チケット料金[前売](税込)＞</h2>
+
+    <p>
+      全席指定6,000円/ 2日間通し券10,000円
+      <br />
+      ファミリー4,500円/ 2日間通し券9,000円
+    </p>
+
+    <h2 class="h2-title">＜チケット最速先行受付＞</h2>
+
+    <p>
+      期間：10月2日(土)18:00~10月17日(日)23:59
+      <br />
+      当落・チケット代支払い期間：2021年10月23日(土)18:00~10月26日(火)21:00
+      <br />
+      受付URL:
+      <a href="https://eplus.jp/pmarusama/" target="_blank">https://eplus.jp/pmarusama/</a>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import url('@/css/information.css');
+</style>
