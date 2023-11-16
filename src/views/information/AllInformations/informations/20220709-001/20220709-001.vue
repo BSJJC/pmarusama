5739b35cb83b61c19fd66a3ad574f9dac3fb9405
BSJJC <805086021@qq.com>
2023-11-17 01:16:20 +0800

    chore

---
+++ src/views/information/AllInformations/informations/20220709-001/20220709-001.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="info-container">
+    <span class="info-date">2022.07.09</span>
+
+    <h1 class="h1-title">2ndフルアルバム『ラブホリック』9月28日(水)発売決定！</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
