56b7a8c4e95ddd001a86db28f2431436b63628cf
BSJJC <805086021@qq.com>
2023-11-29 18:59:15 +0800

    feat: image-component

--- src/views/information/AllInformations/common-components/images-component.vue
+++ src/views/information/AllInformations/common-components/images-component.vue
@@ -1,8 +1,22 @@
 <template>
-  <div></div>
+  <div :class="props.data?.className">
+    <img v-for="(i, index) in props.data?.images" :key="index" :src="i.url" :alt="i.alt" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+type TData = {
+  className: string;
+  images: Array<{
+    url: string;
+    alt: string;
+  }>;
+};
+
+const props = defineProps({
+  data: Object as () => TData,
+});
+</script>
 
 <style scoped>
 @import url('@/css/information.css');
